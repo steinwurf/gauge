b3698367edd64f9babbb9e0e39d383fdd8a72769
Morten V. Pedersen <morten@mortenvp.dk>
2012-07-02 03:39:39 +0200

    adding new benchmark example showing how run can be used outside the template

---
+++ examples/sample_benchmarks/run_outside_benchmark.cpp
@@ -0,0 +1,53 @@
+#include <gauge/gauge.h>
+#include <list>
+#include <vector>
+
+
+template<class Container>
+uint32_t max_container(const Container &one)
+{
+    uint32_t max = 0;
+
+    // RUN may be called outside the BENCHMARK macro as long a we
+    // only call it once
+    RUN{
+
+        for (auto it = one.begin(); it != one.end(); it++)
+        {
+            if(*it > max)
+                max = *it;
+        }
+    }
+
+    return max;
+}
+
+template<typename Container>
+uint32_t setup_and_run()
+{
+    uint32_t elements = 10000;
+    Container c(elements);
+
+    for(auto it = c.begin(); it != c.end(); ++it)
+        *it = rand();
+
+    return max_container(c);
+}
+
+
+BENCHMARK(RunOutside, VectorMaxValue, 10)
+{
+    uint32_t max = setup_and_run< std::vector<uint32_t> >();
+    (void)max;
+}
+
+
+BENCHMARK(RunOutside, ListMaxValue, 10)
+{
+    uint32_t max = setup_and_run< std::list<uint32_t> >();
+    (void)max;
+}
+
+
+
+
