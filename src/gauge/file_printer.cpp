9f3f83f64a11068e102714226b26683889c7e81e
Jeppe Pihl <jpihl08@gmail.com>
2013-09-30 16:47:36 +0200

    added file printer

---
+++ src/gauge/file_printer.cpp
@@ -0,0 +1,74 @@
+#include <cassert>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+#include <boost/program_options.hpp>
+#include <tables/table.hpp>
+
+#include "benchmark.hpp"
+#include "file_printer.hpp"
+#include "runner.hpp"
+
+namespace gauge
+{
+    file_printer::file_printer(const std::string& name,
+        const std::string& extension)
+    {
+        std::stringstream default_filename_ss;
+        default_filename_ss << "out." << extension;
+
+        std::stringstream filename_option_ss;
+        filename_option_ss << extension << "file";
+        m_filename_option = filename_option_ss.str();
+
+        std::stringstream filename_option_desc_ss;
+        filename_option_desc_ss << "Set the output name of the "
+                                << name << " printer";
+
+        // Add the filename option for this printer
+        gauge::po::options_description options;
+
+        auto default_filename_value =
+            gauge::po::value<std::string>()->default_value(
+                default_filename_ss.str());
+
+        options.add_options()
+            (m_filename_option.c_str(), default_filename_value,
+             filename_option_desc_ss.str().c_str());
+
+        gauge::runner::instance().register_options(options);
+    }
+
+    void file_printer::benchmark_result(const benchmark &info,
+                                          const tables::table &results)
+    {
+        tables::table r = results;
+        if(info.has_configurations())
+        {
+            const auto& c = info.get_current_configuration();
+            for(const auto& v : c)
+            {
+                r.add_column(v.first, v.second);
+            }
+        }
+
+        m_tables.insert(m_tables.end(), r);
+    }
+
+    void file_printer::end()
+    {
+        std::ofstream result_file;
+        result_file.open(m_filename, std::ios::trunc);
+        print_to_stream(result_file);
+        result_file.close();
+    }
+
+    void file_printer::set_options(po::variables_map& options)
+    {
+        m_filename = options[m_filename_option].as<std::string>();
+        assert(!m_filename.empty());
+    }
+}
+
---
+++ src/gauge/file_printer.hpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <ostream>
+
+#include <boost/program_options.hpp>
+#include <tables/table.hpp>
+
+#include "printer.hpp"
+#include "benchmark.hpp"
+
+
+namespace gauge
+{
+
+    /// A python result printer implementation. The purpose
+    /// of this printer is to "dump" results to a .py file
+    /// which then may be easily used in python scripts for
+    /// plotting etc.
+    class file_printer : public printer
+    {
+    public:
+
+        /// Create a new python printer
+        /// @param name The name of the printer type
+        /// @param extension The file extension of the result file
+        file_printer(const std::string& name,
+                     const std::string& extension);
+
+    public: // From printer
+
+        /// @see printer::benchmark_result(const benchmark&,const table&)
+        virtual void benchmark_result(const benchmark& info,
+            const tables::table& results);
+
+        /// @see printer::end()
+        virtual void end();
+
+        /// @see printer::set_options(po::variables_map&);
+        virtual void set_options(po::variables_map& options);
+
+    public:
+
+        /// Prints data to a stream. Used in the file_printer::end method.
+        /// @param s Steam to write to.
+        virtual void print_to_stream(std::ostream &s) = 0;
+
+    private:
+
+        /// Store the filename of the output file
+        std::string m_filename;
+
+        /// Store the name of the filename option
+        std::string m_filename_option;
+
+        /// The output tables
+        std::vector<tables::table> m_tables;
+
+    };
+
+}
